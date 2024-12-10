94f28924022bbe52619e649876ba8d25d0a0cf5d
JossMikee <josemiguelvazquezsanchez682@gmail.com>
2024-12-10 05:26:01 -0600

    Nueva version

--- app/routes/informacionDocumentada.py
+++ app/routes/informacionDocumentada.py
@@ -1,20 +1,20 @@
-from .utils.utils import get_db_connection, paginador1, allowed_username
-from flask import Blueprint, render_template, request, redirect, url_for, flash, Flask
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for, flash
+from .utils.utils import get_db_connection
 
 app = Flask(__name__)
 
 @app.route('/guardar_documento', methods=['POST'])
 def guardar_documento():
-    # Obtener los datos del formulario
+    # Obtener los datos del formulario. Cada campo está dentro de un array debido al índice
     documentos = request.form.getlist('documentos[0][codigoDocumento]')
     revision = request.form.getlist('documentos[0][numeroRevision]')
     fecha_emision = request.form.getlist('documentos[0][fechaEmision]')
     fecha_revision = request.form.getlist('documentos[0][fechaRevision]')
-
-    # Puedes crear un array de diccionarios para representar todos los documentos
+    
+    # Crear un array de diccionarios
     documentos_data = []
     
+    # Aquí estamos asumiendo que los campos se envían como listas y tienen la misma longitud
     for i in range(len(documentos)):
         documento = {
             'codigoDocumento': documentos[i],
@@ -23,18 +23,23 @@ def guardar_documento():
             'fechaRevision': fecha_revision[i]
         }
         documentos_data.append(documento)
-
-    # Ahora `documentos_data` es un array de diccionarios con los datos de los documentos
-    print(documentos_data)  # Solo para comprobar los datos recibidos
-    array = request.form['documentos_data']
     
+    # Mostrar los datos en consola (esto solo para verificar)
+    print(documentos_data)
+    
+    # Insertar los datos en la base de datos
     con = get_db_connection()
     cur = con.cursor()
-    sql = "INSERT INTO documentos (array) VALUES %s"
-    valores =(array)
-    cur.execute(sql,valores)
+    
+    # Preparamos una consulta para insertar múltiples registros
+    for doc in documentos_data:
+        sql = "INSERT INTO documentos (codigo_documento, numero_revision, fecha_emision, fecha_revision) VALUES (%s, %s, %s, %s)"
+        valores = (doc['codigoDocumento'], doc['numeroRevision'], doc['fechaEmision'], doc['fechaRevision'])
+        cur.execute(sql, valores)
+    
     con.commit()
     cur.close()
     con.close()
-    flash('Usuario agregado correctamente')
+    
+    flash('Documento guardado correctamente')
     return redirect(url_for('informacionDocumentada.infoDocu'))
